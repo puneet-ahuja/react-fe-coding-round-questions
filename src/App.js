4e7208a7c1b7bbccd017f6785008873ebfac843b
Puneet Ahuja <ahujapuneet92@gmail.com>
2024-05-01 10:49:51 +0530

    Setup React ROuter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordian from "./components/Accordian/Accordian";
+import Home from "./components/Home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// TODO : Need to understand why we are using createBrowserRouter?
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Home></Home>
+    ),
+  },
+  {
+    path: "accordian",
+    element: <Accordian></Accordian>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <RouterProvider router={router} />
   );
 }
 
