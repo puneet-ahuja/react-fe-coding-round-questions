59354add132ee7e335fc835fdb8da879b4a39919
Puneet Ahuja <ahujapuneet92@gmail.com>
2024-05-11 11:30:35 +0530

    Added Star Rating Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import RandomColorGenerator from "./components/RandomColorGenerator/RandomColorGenerator";
+import StarRating from "./components/StarRating/StarRating";
 
 // TODO : Need to understand why we are using createBrowserRouter?
 const router = createBrowserRouter([
@@ -18,10 +19,13 @@ const router = createBrowserRouter([
     path: "accordian",
     element: <Accordian></Accordian>,
   },
-
   {
     path: "random-color-generator",
     element: <RandomColorGenerator></RandomColorGenerator>,
+  },
+  {
+    path: "star-rating",
+    element: <StarRating noOfStars={10}></StarRating>,
   },
   
 ]);
